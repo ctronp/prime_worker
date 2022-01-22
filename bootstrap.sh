ff1e60f3df11b99e4e035cb34dad1aaa98f96ff9
ctronp <carlotp7@uc.cl>
2022-01-22 15:06:43 -0300

    compile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 cd "$(dirname "$0")" || exit
-cargo build --release --target aarch64-unknown-linux-gnu
+cross build --release  --target aarch64-unknown-linux-gnu
 cp target/release/api bootstrap
